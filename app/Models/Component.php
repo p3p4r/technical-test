4f45bad98a73795b9be6b68999abf10c0b0739eb
P3P4R <2181857@my.ipleiria.pt>
2023-09-05 16:59:33 +0100

    added missing relationships in component

--- app/Models/Component.php
+++ app/Models/Component.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Component extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function turbines()
+    {
+        return $this->belongsToMany(Turbine::class, 'turbine_components')
+            ->withPivot('grade_id');
+    }
+
+    public function grade()
+    {
+        return $this->belongsTo(Grade::class);
+    }
 }
